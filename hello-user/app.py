17e51cba5794c2f994b29ab49637d7f2e94af62a
woogieReal <woogiereal@gmail.com>
2024-01-25 17:34:54 +0900

    feat: 파이썬 flask 기본 앱 생성

---
+++ hello-user/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
