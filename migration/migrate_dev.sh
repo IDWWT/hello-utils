162edf450940df9e581f5e2fdad01fb09f92779c
woogieReal <woogiereal@gmail.com>
2024-01-28 15:57:16 +0900

    [인프라][마이그레이션] 스크립트 refactor

--- migration/migrate_dev.sh
+++ migration/migrate_dev.sh
@@ -1,13 +1,4 @@
-VERSION=1.0.0
-IMAGE_NAME="hello-migration-image"
-IMAGE_NAME_WITH_TAG="${IMAGE_NAME}:${VERSION}"
-
-# Docker 이미지가 존재하는지 확인
-if docker image inspect $IMAGE_NAME_WITH_TAG &> /dev/null; then
-  echo "Image $IMAGE_NAME already exists."
-else
-  docker build -t $IMAGE_NAME_WITH_TAG ./migration
-fi
+IMAGE_NAME_WITH_TAG=$(sh $(pwd)/migration/build_image.sh)
 
 docker run \
   --rm \
