d41af19b599e7b59d874972699f70adaafec7d26
ro-ssang <alakswk43@gmail.com>
2024-01-31 05:44:47 +0900

    [인프라][자바] spring boot 애플리케이션 컨터이너화

---
+++ hello-code/src/test/java/com/helloutils/hellocode/HellocodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloutils.hellocode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellocodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
