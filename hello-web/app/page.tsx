4ecee5c021a507d739b9b49bc252f7f8e93d0b78
woogieReal <woogiereal@gmail.com>
2024-01-31 22:28:21 +0900

    [웹] 깃허브 로그인  기능 추가

--- hello-web/app/page.tsx
+++ hello-web/app/page.tsx
@@ -1,7 +1,14 @@
-export default function Home() {
+import { SignIn, SignOut } from '@/components/auth-components'
+import { auth } from "@/auth"
+
+const Home = async () => {
+  const session = await auth()
+
   return (
     <div>
-
+      {session?.user ? <SignOut />: <SignIn />}
     </div>
   );
 }
+
+export default Home
