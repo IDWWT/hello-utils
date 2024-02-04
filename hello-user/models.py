41f132518d3b12922f470df221602c73f59193d0
woogieReal <woogiereal@gmail.com>
2024-02-04 23:21:51 +0900

    [파이썬] orm class 정의

---
+++ hello-user/models.py
@@ -0,0 +1,33 @@
+import enum
+from database import Base
+from sqlalchemy import Column, DateTime, ForeignKey, String, Enum
+from sqlalchemy.sql import func
+from sqlalchemy.orm import backref, relationship
+import uuid
+
+class YN(enum.Enum):
+    Y = 'Y'
+    N = 'N'
+
+SA_YN = Enum(YN)
+
+class UserRole(Base):
+    __tablename__ = "user_role"
+    role_code = Column(String(30), primary_key=True)
+    role_name = Column(String(50), nullable=False)
+    can_edit_post_yn = Column(SA_YN, nullable=False)
+    can_delete_post_yn = Column(SA_YN, nullable=False)
+    created_at = Column(DateTime, nullable=False, server_default=func.now())
+    updated_at = Column(DateTime, nullable=False, server_default=func.now())
+
+class UserMaster(Base):
+    __tablename__ = "user_master"
+    user_id = Column(String(36), primary_key=True, default=uuid.uuid4())
+    user_email = Column(String(320), nullable=False, unique=True)
+    role_code = Column(String(30), ForeignKey("user_role.role_code"), nullable=False, default="NORMAL")
+    social_id = Column(String(255))
+    created_at = Column(DateTime, nullable=False, server_default=func.now())
+    updated_at = Column(DateTime, nullable=False, server_default=func.now())
+    user_role = relationship(
+        UserRole, backref=backref("user_role", uselist=True, cascade="save-update")
+    )
