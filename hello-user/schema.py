fceb618413765ac031f1f62632738af2515b5057
woogieReal <woogiereal@gmail.com>
2024-02-08 23:55:31 +0900

    [파이썬] 주석 추가 및 코드 리팩터링

--- hello-user/schema.py
+++ hello-user/schema.py
@@ -6,6 +6,7 @@ from database import db_session
 import uuid
 
 class UserRole(SQLAlchemyObjectType):
+    # 내부 클래스로서, 모델과 GraphQL 객체 사이의 매핑 및 추가 설정을 정의하는 데 사용
     class Meta:
         model = UserRoleModel
 
@@ -14,10 +15,12 @@ class UserMaster(SQLAlchemyObjectType):
         model = UserMasterModel
 
 class UserMasterMutation(graphene.Mutation):
+    # GraphQL Mutation의 입력 파라미터를 정의하는데 사용
     class Arguments:
         user_email = graphene.String(required=True)
     
-    user = graphene.Field(lambda: UserMaster)
+    # GraphQL 쿼리에서 반환될 데이터의 타입과 구조를 결정
+    user = graphene.Field(UserMaster)
 
     def mutate(self, info, user_email):
         user = db_session.query(UserMasterModel).filter_by(user_email=user_email).first()
@@ -37,14 +40,22 @@ class Query(graphene.ObjectType):
         role_code=graphene.String(required=False),
     )
 
-    def resolve_users(self, info, user_id=None, user_email=None, role_code=None):
+    def resolve_users(self, info, **kwargs):
+        # info: GraphQL의 실행 컨텍스트와 쿼리 정보에 접근하고 제어하는 데 사용
         query = UserMaster.get_query(info)
 
-        if user_id: query = query.filter(UserMasterModel.user_id == user_id)
-        if user_email: query = query.filter(UserMasterModel.user_email == user_email)
-        if role_code: query = query.filter(UserMasterModel.role_code == role_code)
+        if kwargs.get("user_id"):
+            query = query.filter(UserMasterModel.user_id == kwargs.get("user_id"))
+
+        if kwargs.get("user_email"):
+            query = query.filter(UserMasterModel.user_email == kwargs.get("user_email"))
+
+        if kwargs.get("role_code"):
+            query = query.filter(UserMasterModel.role_code == kwargs.get("role_code"))
+
 
         return query.all()
+
 
 class Mutation(graphene.ObjectType):
     mutate_user = UserMasterMutation.Field()
