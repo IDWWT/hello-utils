a9822b7c85f4e7237a696a5100d3032672a68efb
HAN-SEOHYUN <000312@daum.net>
2024-02-16 05:56:28 +0900

    [JAVA] Util Entity 생성 reolve #50

---
+++ hello-code/src/main/java/com/helloutils/hellocode/domain/Util.java
@@ -0,0 +1,56 @@
+package com.helloutils.hellocode.domain;
+
+import com.helloutils.hellocode.enums.CategoryCd;
+import com.helloutils.hellocode.enums.LanguageCd;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Util {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    private String description;
+
+    @Column(length = 3000)
+    private String code;
+
+    private Long userId;
+
+    @Enumerated(EnumType.STRING)
+    private LanguageCd languageCd;
+
+    @Enumerated(EnumType.STRING)
+    private CategoryCd categoryCd;
+
+    private LocalDateTime createdAt;
+
+    @Builder
+    public Util(String title,
+                String description,
+                String code,
+                Long userId,
+                LanguageCd languageCd,
+                CategoryCd categoryCd) {
+        this.title = title;
+        this.description = description;
+        this.code = code;
+        this.userId = userId;
+        this.languageCd = languageCd;
+        this.categoryCd = categoryCd;
+        this.createdAt = LocalDateTime.now();
+    }
+
+}
