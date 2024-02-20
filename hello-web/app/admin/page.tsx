fe642c210b59ea8d08ffa0c80a2eeac3b71dca31
woogieReal <woogiereal@gmail.com>
2024-02-21 00:32:39 +0900

    [NextJS] 관리자 페이지 추가

---
+++ hello-web/app/admin/page.tsx
@@ -0,0 +1,17 @@
+import { auth } from "@/auth"
+import { useSession, getSession } from "next-auth/react"
+
+export default async function Header() {
+  const session = await auth();
+  const user = session?.user;
+
+  console.log(user);
+  if (!user) return "Please Sign In";
+  if (!user.isAdmin) return "Not Allowed User";
+
+  return (
+    <div>
+      <h1>ADMIN PAGE</h1>
+    </div>
+  )
+}
