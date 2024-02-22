75265fb78b10660ef440926c63871c8148a4ddba
woogieReal <woogiereal@gmail.com>
2024-02-22 23:26:09 +0900

    feat: 테스트 코드 수정

--- hello-user/test_graphql.py
+++ hello-user/test_graphql.py
@@ -1,49 +1,67 @@
 from graphene.test import Client
 from schema import schema as my_schema
-from database import db_session, init_db
-from models import UserRole, UserMaster
-import uuid
 import inspect
 import json
 
-# 스키마 초기화
-init_db()
-
-user_id_01, user_id_02 = uuid.uuid4(), uuid.uuid4()
-normal_user_01 = UserMaster(user_id=user_id_01, user_email='normal_user_01@hello.com')
-normal_user_02 = UserMaster(user_id=user_id_02, user_email='normal_user_02@hello.com')
-db_session.add_all([normal_user_01, normal_user_02])
-db_session.commit()
 
 def users():
     try:
         client = Client(my_schema)
-        response = client.execute('''query Users {
-            users (sortBy: "user_email") {
-                userId
-                userEmail
-            }
-        }''')
-        expect = {
-            "data": {
-                "users": [
-                    {
-                        "userId": f"{user_id_01}",
-                        "userEmail": "normal_user_01@hello.com"
-                    },
-                    {
-                        "userId": f"{user_id_02}",
-                        "userEmail": "normal_user_02@hello.com"
+        response = client.execute('''
+            query Users {
+                users(first: 5) {
+                    totalCount
+                    edges {
+                        node {
+                            userId
+                            userEmail
+                            roleCode
+                            socialId
+                            createdAt
+                            updatedAt
+                            id
+                        }
                     }
-                ]
+                    pageCursors {
+                        around {
+                            cursor
+                            isCurrent
+                            page
+                        }
+                        first {
+                            cursor
+                            isCurrent
+                            page
+                        }
+                        last {
+                            cursor
+                            isCurrent
+                            page
+                        }
+                        next {
+                            cursor
+                            isCurrent
+                            page
+                        }
+                        previous {
+                            cursor
+                            isCurrent
+                            page
+                        }
+                    }
+                }
             }
-        }
-        assert response == expect
+        ''')
+        users_data = response["data"]["users"]
+        
+        assert "totalCount" in users_data
+        assert "edges" in users_data
+        assert "pageCursors" in users_data
         print("\033[94m" + "test success : " + inspect.currentframe().f_code.co_name + "\033[0m")
     except:
         print("\033[91m" + "test failed : " + inspect.currentframe().f_code.co_name + "\033[0m")
         print("response :", json.dumps(response, indent=4))
-        print("expect :", json.dumps(expect, indent=4))
+        # print("expect :", json.dumps(expect, indent=4))
         exit(1)
 
 def mutate_user():
@@ -120,7 +138,7 @@ def mutate_user_with_optional_data():
         exit(1)
 
 
-# users()
+users()
 mutate_user()
 mutate_user_with_optional_data()
 exit(0)
