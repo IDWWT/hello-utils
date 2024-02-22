78866ef34f9afb5b329a698f0a167f07f7cca669
woogieReal <woogiereal@gmail.com>
2024-02-22 22:54:10 +0900

    feat: relay 기능 사용하여 필터링 및 페이지네이션 구현

--- hello-user/schema.py
+++ hello-user/schema.py
@@ -1,13 +1,15 @@
 from operator import and_
 from models import UserMaster as UserMasterModel
 import graphene
-from graphene_sqlalchemy import SQLAlchemyObjectType
+from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
 from database import db_session
-
+from relay_pagination.fields import PageConnection, PageConnectionField
 
 class UserMaster(SQLAlchemyObjectType):
     class Meta:
         model = UserMasterModel
+        interfaces = (graphene.relay.Node,)
+        connection_class = PageConnection
 
 class UserMasterMutation(graphene.Mutation):
     # GraphQL Mutation의 입력 파라미터를 정의하는데 사용
@@ -29,39 +31,13 @@ class UserMasterMutation(graphene.Mutation):
         return UserMasterMutation(user=user)
 
 class Query(graphene.ObjectType):
-    users = graphene.List(
+    users = PageConnectionField(
         UserMaster,
-        user_id=graphene.String(required=False),
-        user_email=graphene.String(required=False),
-        role_code=graphene.String(required=False),
-        sort_by=graphene.String(required=False),
-        sort_order=graphene.String(required=False),
+        user_id=graphene.String(),
+        user_email=graphene.String(),
+        role_code=graphene.String(),
+        social_id=graphene.String(),
     )
-
-    def resolve_users(self, info, **kwargs):
-        # info: GraphQL의 실행 컨텍스트와 쿼리 정보에 접근하고 제어하는 데 사용
-        query = UserMaster.get_query(info)
-
-        if kwargs.get("user_id"):
-            query = query.filter(UserMasterModel.user_id == kwargs.get("user_id"))
-
-        if kwargs.get("user_email"):
-            query = query.filter(UserMasterModel.user_email == kwargs.get("user_email"))
-
-        if kwargs.get("role_code"):
-            query = query.filter(UserMasterModel.role_code == kwargs.get("role_code"))
-        
-        # 정렬
-        if kwargs.get("sort_by"):
-            sort_by = kwargs.get("sort_by")
-            sort_order = kwargs.get("sort_order", "asc")  # 기본적으로 오름차순
-            if sort_order == "asc":
-                query = query.order_by(getattr(UserMasterModel, sort_by).asc())
-            elif sort_order == "desc":
-                query = query.order_by(getattr(UserMasterModel, sort_by).desc())
-
-
-        return query.all()
 
 
 class Mutation(graphene.ObjectType):
