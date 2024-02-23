b510d89d330db2a37443d81c5d75d07ae0a37076
woogieReal <woogiereal@gmail.com>
2024-02-23 14:17:30 +0900

    feat: graphql client 컴포넌트에서도 쓸 수 있도록 설정

---
+++ hello-web/utils/graphql-client.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import {
+  ApolloClient,
+  ApolloLink,
+  HttpLink,
+} from "@apollo/client";
+import {
+  ApolloNextAppProvider,
+  NextSSRApolloClient,
+  NextSSRInMemoryCache,
+  SSRMultipartLink,
+} from "@apollo/experimental-nextjs-app-support/ssr";
+
+function makeClient() {
+  const httpLink = new HttpLink({
+    uri: "http://user.hello-utils.wiki/graphql",
+  });
+
+  return new NextSSRApolloClient({
+    cache: new NextSSRInMemoryCache(),
+    link:
+      typeof window === "undefined"
+        ? ApolloLink.from([
+          new SSRMultipartLink({
+            stripDefer: true,
+          }),
+          httpLink,
+        ])
+        : httpLink,
+  });
+}
+
+export function ApolloWrapper({ children }: React.PropsWithChildren) {
+  return (
+    <ApolloNextAppProvider makeClient={makeClient}>
+      { children }
+    </ApolloNextAppProvider>
+  );
+}
