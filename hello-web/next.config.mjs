3273d42dbab94155f754a113479e95a38f38cd72
woogieReal <woogiereal@gmail.com>
2024-02-23 17:45:23 +0900

    update next config

--- hello-web/next.config.mjs
+++ hello-web/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+        allowedOrigins: ["web.hello-utils.wiki"],
+    },
+  },
+};
 
 export default nextConfig;
