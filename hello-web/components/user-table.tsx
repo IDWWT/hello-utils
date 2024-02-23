2408ef98f33cbc5da2f62db7e2488231019e57d3
woogieReal <woogiereal@gmail.com>
2024-02-23 15:17:07 +0900

    fix: table warning

--- hello-web/components/user-table.tsx
+++ hello-web/components/user-table.tsx
@@ -60,7 +60,7 @@ export default function UserTable() {
           {edges.map((edge, idx) => {
             const { node: user } = edge;
             return (
-              <tr className="bg-white border-b hover:bg-gray-50">
+              <tr key={`each-user-${idx}`} className="bg-white border-b hover:bg-gray-50">
                 {/* 
                   border-b b는 bottom, 아래에 border가 생기게 함
                 */}
@@ -91,12 +91,12 @@ export default function UserTable() {
             <button className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700" disabled={!pageCursors.previous?.cursor} onClick={() => setAfterCursor(pageCursors.previous?.cursor)}>
               <span className="sr-only">Previous</span>
               <svg className="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4" />
               </svg>
             </button>
           </li>
-          {pageCursors.around.map((cursor) => (
-            <li>
+          {pageCursors.around.map((cursor, idx) => (
+            <li key={`each-page-${idx}`}>
               {cursor.isCurrent
                 ? <button aria-current="page" className="z-10 flex items-center justify-center px-4 h-10 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700" onClick={() => setAfterCursor(cursor.cursor)}>{cursor.page}</button>
                 : <button className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700" onClick={() => setAfterCursor(cursor.cursor)}>{cursor.page}</button>
@@ -108,7 +108,7 @@ export default function UserTable() {
             <button className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700" disabled={!pageCursors.next?.cursor} onClick={() => setAfterCursor(pageCursors.next?.cursor)}>
               <span className="sr-only">Next</span>
               <svg className="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
               </svg>
             </button>
           </li>
