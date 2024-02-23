18af62f9d9b3d8e6eefa4503751ee33031255fc2
ro-ssang <alakswk43@gmail.com>
2024-02-23 15:01:24 +0900

    [인프라] 프로젝트 실행시 인증서 생성하도록 변경 resolve #81

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,9 @@
 # data 디렉토리 생성
 source "$(pwd)/scripts/create-data-directory.sh"
 
+# https 인증서 생성
+source "$(pwd)/scripts/create-certificates.sh"
+
 # .env 파일 다운로드
 doppler secrets download --no-file --format docker --project hello-code --config dev > ./hello-code/.env
 doppler secrets download --no-file --format docker --project hello-user --config dev > ./hello-user/.env
