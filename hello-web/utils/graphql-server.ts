1ad8bb1ab53b94112dfbaea7b2cab52c6648e2ac
woogieReal <woogiereal@gmail.com>
2024-03-05 23:54:54 +0900

    [Next.js] graphql server client 헤더에 유저 토큰 정보 추가

--- hello-web/utils/graphql-server.ts
+++ hello-web/utils/graphql-server.ts
@@ -1,5 +1,7 @@
+import { UserAccessToken } from "@/types/user";
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
+import { cookies } from "next/headers";
 
 /**
  * middleware 에서 아래 에러 발생
@@ -26,10 +28,22 @@ import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rs
 //   });
 // });
 
-export const getClient = () => new ApolloClient({
-  cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: `${process.env.HELLO_BFF_API_URL}/graphql`,
-    fetchOptions: { cache: "no-store" },
-  }),
-});
+export const getClient = () => {
+  const keys: (keyof UserAccessToken)[] = ['userId', 'accessToken'];
+  const userToken = (keys as string[]).reduce((acc, cur) => {
+    acc[cur] = cookies().get(cur)?.value;
+    return acc;
+  }, {} as { [index: string]: string | undefined });
+
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+    link: new HttpLink({
+      headers: {
+        "X-User-Id": cookies().get("userId")?.value || "",
+        "X-User-Token": cookies().get("accessToken")?.value || "",
+      },
+      uri: `${process.env.HELLO_BFF_API_URL}/graphql`,
+      fetchOptions: { cache: "no-store" },
+    }),
+  });
+}
